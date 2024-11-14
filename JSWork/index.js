acae641e079aa5988572cff7b1729baf5439b2b0
remarkableraj <raj841226singh@gmail.com>
2024-11-14 13:01:53 +0530

    rect

---
+++ JSWork/index.js
@@ -0,0 +1,65 @@
+// console.log("welcome to js");
+// // var a=12;
+// const a=12;
+// console.log(a);
+// if(a>10){
+//     // var b=23;
+//     let b=23;
+//     a=40;
+
+//     console.log("a inside condition:"+a);
+// }
+// console.log("b outside condition:"+b);
+
+// let a=12;
+// let b="12";
+// console.log(typeof b);
+
+// let today=Date();
+// console.log(today);
+
+// let a=12;
+// let b="12";
+// if(a==b){
+//     console.log("welcome");
+// }
+// else{
+//     console.log("Hello");
+// }
+// let myname="Amit";
+// let college="ABES EC";
+// let result='Hi,my name is $(myname) and I am doing engineering from $(college)';
+// console.log(result);
+
+// // object in js
+// const students={
+//     sname:"Amit",
+//     course:"B.Tech",
+//     college:"ABES"
+
+
+// }
+// console.log("Name:"+students.sname+" course:"+students.course);
+// // or
+// const {sname}=students;
+// console.log(sname);
+// console.log("Helllo world");
+// const div=document.getElementsByClassName("parent")
+// console.log(div);
+// div[0].innerHTML="<h2 style=color:red>Hello JS</h2>"
+// const h1=document.createElement("h1");
+// console.log(h1);
+// h1.innerText="ABES Engineering college";
+// h1.style.padding="20px";
+// h1.style.marginleft="300px";
+// div[0]
+// ;
+// function getData(){
+//     console.log("Hi,inside getData function");
+//     div[0].innerHTML="<h2 style=color:red>Hello, Event handled</h2>";
+// }
+
+// const button=document.getElementsByClassName("btn");
+// console.log(button);
+// button[0].addEventListener("click",getData);
+fetch("https://dummyjson.com/products");
